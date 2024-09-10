002e62c6ef502c197770a71d9857974c16d53439
Muhsin <normanbellenger@protonmail.com>
2024-09-10 19:20:45 +0200

    added quil editor

--- thesiswizard/lib/widgets/wysiwyg_editor.dart
+++ thesiswizard/lib/widgets/wysiwyg_editor.dart
@@ -1,16 +1,41 @@
 // lib/widgets/wysiwyg_editor.dart
 import 'package:flutter/material.dart';
+import 'package:thesiswizard/widgets/colors.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:flutter_math_fork/flutter_math.dart';
 
-class WysiwygEditor extends StatelessWidget {
+class WysiwygEditor extends StatefulWidget {
+  @override
+  _WysiwygEditorState createState() => _WysiwygEditorState();
+}
+
+class _WysiwygEditorState extends State<WysiwygEditor> {
+  final quill.QuillController _controller = quill.QuillController.basic();
+  String _filename = 'untitled';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black87,
-      child: Center(
-        child: Text(
-          'WYSIWYG and LaTeX Editor',
-          style: TextStyle(color: Colors.white),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_filename, style: TextStyle(color: AppColors.text)),
+        backgroundColor: AppColors.primary,
+      ),
+      body: Column(
+        children: [
+          // Toolbar for text formatting
+          quill.QuillToolbar.simple(controller: _controller),
+
+          // The actual editor
+          Expanded(
+            child: Container(
+              color: AppColors.background,
+              padding: EdgeInsets.all(8.0),
+              child: quill.QuillEditor.basic(
+                controller: _controller,
+                // readOnly: false, // Editor is interactive
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- thesiswizard/macos/Flutter/GeneratedPluginRegistrant.swift
+++ thesiswizard/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
+import device_info_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
