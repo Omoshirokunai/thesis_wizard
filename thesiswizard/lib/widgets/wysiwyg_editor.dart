bd70be28a2a86ca33fece94139b185bd6b261dc6
Omoshirokunai <normanbellenger@protonmail.com>
2024-09-12 18:55:34 +0200

    updated editor

--- thesiswizard/lib/widgets/wysiwyg_editor.dart
+++ thesiswizard/lib/widgets/wysiwyg_editor.dart
@@ -1,8 +1,7 @@
 // lib/widgets/wysiwyg_editor.dart
 import 'package:flutter/material.dart';
-import 'package:thesiswizard/widgets/colors.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
-import 'package:flutter_math_fork/flutter_math.dart';
+import 'package:thesiswizard/widgets/colors.dart';
 
 class WysiwygEditor extends StatefulWidget {
   @override
@@ -12,31 +11,47 @@ class WysiwygEditor extends StatefulWidget {
 class _WysiwygEditorState extends State<WysiwygEditor> {
   final quill.QuillController _controller = quill.QuillController.basic();
   String _filename = 'untitled';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_filename, style: TextStyle(color: AppColors.text)),
-        backgroundColor: AppColors.primary,
-      ),
-      body: Column(
-        children: [
-          // Toolbar for text formatting
-          quill.QuillToolbar.simple(controller: _controller),
-
-          // The actual editor
-          Expanded(
-            child: Container(
-              color: AppColors.background,
-              padding: EdgeInsets.all(8.0),
-              child: quill.QuillEditor.basic(
-                controller: _controller,
-                // readOnly: false, // Editor is interactive
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: AppColors.accent,
+          body: Column(
+            children: [
+              // The rich text editor
+              Expanded(
+                child: Container(
+                  color: Colors.black87,
+                  padding: EdgeInsets.all(16.0),
+                  child: quill.QuillEditor.basic(
+                    controller: _controller,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+        // Floating toolbar for text formatting
+        Positioned(
+          bottom: 50,
+          left: 20,
+          right: 20,
+          child: Container(),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+              setState(() {});
+            },
+            backgroundColor: Colors.black54,
+            child: Icon(Icons.format_bold, color: Colors.white),
+          ),
+        ),
+      ],
     );
   }
 }
